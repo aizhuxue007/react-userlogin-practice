3b097cb8b3d90f33346629345a030d1db8548011
aizhuxue007 <aizhuprogram@gmail.com>
2023-07-15 08:57:34 -0400

    basic user authentication with no hashing. Email and pw stored in localStorage

--- userlogin-practice/src/App.jsx
+++ userlogin-practice/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import SignIn from "./SignIn";
+import { useEffect } from "react";
+import "./App.css";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const demoEmail = 'aizhu@gmail.com'
+  const demoPwd = 'hotdog123'
+  const user = {
+    email: demoEmail,
+    pwd: demoPwd
+  }
+
+  useEffect(() => {
+    localStorage.setItem('email', user.email)
+    localStorage.setItem('pwd', user.pwd)
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main-container w-full h-screen flex justify-center items-center text-white font-sans">
+        <SignIn />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ userlogin-practice/src/SignIn.jsx
@@ -0,0 +1,108 @@
+import { useRef, useState, useEffect } from "react";
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+
+  useEffect(() => {
+    console.log(`success: ${success}`)
+  }, [success])
+
+  const submitLogin = async (e) => {
+    e.preventDefault();
+    handleLogin()
+  };
+
+  const handleLogin = () => {
+    consoleLogin()
+    resetLoginForm()
+    authenticateLogin()
+  }
+
+  const consoleLogin = () => {
+    console.log(`email input: ${email}`);
+    console.log(`password input: ${pwd}`);
+  }
+
+  const resetLoginForm = () => {
+    setEmail("");
+    setPwd("");
+  }
+
+  const authenticateLogin = () => {
+    const storedEmail = localStorage.getItem('email')
+    const storedPassword = localStorage.getItem('pwd')
+    console.log(storedEmail===email, storedPassword===pwd)
+    if (email === storedEmail && pwd === storedPassword) {
+        setSuccess(true)
+        setErrMsg('')
+    } else {
+        setErrMsg('Wrong email or password')
+    }
+  }
+
+  return (
+    <section className="card box-border w-3/5 min-h-fit bg-red-500 rounded-md p-10">
+        
+      {success ? (
+        <div className="text-white">Success Login!</div>
+      ) : (
+        <>
+          <h1 className="text-white text-2xl font-serif">Sign In</h1>
+          {errMsg && <><p className="my-5 text-yellow-200">{errMsg}</p></>}
+          <form className="mt-5 flex justify-center" onSubmit={submitLogin}>
+            <div className="w-full h-full flex-col justify-between">
+              <div className="input-container">
+                <div className="email-input w-full mb-5 flex justify-center items-center">
+                  {/* <label htmlFor="email" className="mr-12">
+                    Email:
+                  </label> */}
+                  <input
+                    className="rounded-md p-2 text-red-700 placeholder:text-red-400"
+                    type="email"
+                    autoComplete="off"
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    required
+                  />
+                </div>
+                <div className="password-input w-full flex justify-center items-center">
+                  {/* <label htmlFor="password" className="label mr-5">
+                    Password:
+                  </label> */}
+                  <input
+                    className="rounded-md p-2 text-red-700 placeholder:text-red-400"
+                    type="password"
+                    placeholder="Password"
+                    value={pwd}
+                    onChange={(e) => {
+                      setPwd(e.target.value);
+                    }}
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="form-buttons mt-10">
+                <button
+                  className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 mr-5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                  type="submit"
+                >
+                  Sign Up
+                </button>
+                <button className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                  Login
+                </button>
+              </div>
+            </div>
+          </form>
+        </>
+      )}
+    </section>
+  );
+};
+
+export default SignIn;
